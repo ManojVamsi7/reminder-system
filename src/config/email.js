3035e0b78557e488c7d5114490bb86aae3f0b4cf
Manoj Vamsi <vamsidakamarri07@gmail.com>
2026-02-12 16:14:55 +0530

    Fix ENETUNREACH error on Render by switching to port 587

--- src/config/email.js
+++ src/config/email.js
@@ -2,8 +2,11 @@ const nodemailer = require('nodemailer');
 const config = require('./env');
 
 // Create email transporter
+// Using explicit host and port 587 (STARTTLS) because port 465 often has IPv6/ENETUNREACH issues on Render
 const transporter = nodemailer.createTransport({
-    service: config.email.service,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: config.email.user,
         pass: config.email.password,
